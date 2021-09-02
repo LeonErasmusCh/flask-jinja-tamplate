1007b42996a9d8aa1f8fcdcd1b68dd0d73ce92f7
Leon Erasmus <leon@santiagobiltong.cl>
2021-09-01 20:04:56 -0400

    finish delete button CSS next and done

--- website/views.py
+++ website/views.py
@@ -1,27 +1,27 @@
-from flask import Blueprint, render_template, request, flash, jsonify 
-from flask_login import login_required,  current_user
-from flask_login import login_required
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
 from .models import Note
 from . import db
+import json
 
 views = Blueprint('views', __name__)
+
 
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    if request.method == "POST":
+    if request.method == 'POST':
         note = request.form.get('note')
+
         if len(note) < 1:
             flash('Note is too short!', category='error')
         else:
-            new_note = Note(data=note, user_id=current_user.id )
+            new_note = Note(data=note, user_id=current_user.id)
             db.session.add(new_note)
-            db.session.commit(  )
-            flash('note added', category='success')
+            db.session.commit()
+            flash('Note added!', category='success')
 
-    return render_template("home.html" ,user=current_user)
-
-
+    return render_template("home.html", user=current_user)
 
 
 @views.route('/delete-note', methods=['POST'])
@@ -35,5 +35,3 @@ def delete_note():
             db.session.commit()
 
     return jsonify({})
-
-
